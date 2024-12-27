1d1df16d95d9d03ad48ac5d8138ce71aa125c020
BugHunter-X <askzenai@gmail.com>
2024-12-27 10:52:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+// Some irrelevant code
+// ...
+
+function myComponent() {
+  return (
+    <div className="bg-gradient-to-r from-blue-500 to-purple-500">
+      {/* ... */}
+    </div>
+  );
+}
+```
