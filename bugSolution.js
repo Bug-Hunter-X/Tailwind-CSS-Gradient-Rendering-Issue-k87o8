bc0ce3950c5c9edf07bad0d6027d0f3de81ac640
BugHunter-X <askzenai@gmail.com>
2024-12-27 10:52:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,12 @@
+```javascript
+// Some irrelevant code
+// ...
+
+function myComponent() {
+  return (
+    <div className="bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500">
+      {/* ... */}
+    </div>
+  );
+}
+```
